2c973c28268222459509bc82e51fa156eada08c1
Michael Davis <m.r.davis@me.com>
2021-06-15 21:59:42 +0100

    Add advanced skill data

---
+++ src/wfrp/character/skill_data.py
@@ -0,0 +1,66 @@
+BASIC_SKILL_DATA = {
+    "Art": {
+        "characteristic": ("dexterity", "Dex"),
+    },
+    "Athletics": {
+        "characteristic": ("agility", "Ag"),
+    },
+    "Bribery": {
+        "characteristic": ("fellowship", "Fel"),
+    },
+    "Charm": {"characteristic": ("fellowship", "Fel")},
+    "Charm Animal": {"characteristic": ("willpower", "WP")},
+    "Climb": {"characteristic": ("strength", "S")},
+    "Cool": {"characteristic": ("willpower", "WP")},
+    "Consume Alcohol": {"characteristic": ("toughness", "T")},
+    "Dodge": {"characteristic": ("agility", "Ag")},
+    "Drive": {"characteristic": ("agility", "Ag")},
+    "Endurance": {"characteristic": ("toughness", "T")},
+    "Entertain": {"characteristic": ("fellowship", "Fel")},
+    "Gamble": {"characteristic": ("intelligence", "Int")},
+    "Gossip": {"characteristic": ("fellowship", "Fel")},
+    "Haggle": {"characteristic": ("fellowship", "Fel")},
+    "Intimidate": {"characteristic": ("strength", "S")},
+    "Intuition": {"characteristic": ("initiative", "I")},
+    "Leadership": {"characteristic": ("fellowship", "Fel")},
+    "Melee": {"characteristic": ("weapon_skill", "WS")},
+    "Navigation": {"characteristic": ("initiative", "I")},
+    "Outdoor Survival": {"characteristic": ("intelligence", "Int")},
+    "Perception": {"characteristic": ("initiative", "I")},
+    "Ride": {"characteristic": ("agility", "Ag")},
+    "Row": {"characteristic": ("strength", "S")},
+    "Stealth": {"characteristic": ("agility", "Ag")},
+}
+
+ADVANCED_SKILL_DATA = {
+    "Animal Care": {
+        "characteristic": ("intelligence", "Int"),
+    },
+    "Animal Training": {
+        "characteristic": ("intelligence", "Int"),
+        "specialisations": ["Demigryph", "Dog", "Horse", "Pegasus", "Pigeon"],
+    },
+    "Channelling": {
+        "characteristic": ("willpower", "WP"),
+    },
+    "Evaluate": {"characteristic": ("intelligence", "Int")},
+    "Heal": {"characteristic": ("intelligence", "Int")},
+    "Language": {"characteristic": ("intelligence", "Int")},
+    "Lore": {"characteristic": ("intelligence", "Int")},
+    "Perform": {"characteristic": ("agility", "Ag")},
+    "Pick Lock": {"characteristic": ("dexterity", "Dex")},
+    "Play": {"characteristic": ("dexterity", "Dex")},
+    "Pray": {"characteristic": ("fellowship", "Fel")},
+    "Ranged": {"characteristic": ("ballistic_skill", "BS")},
+    "Research": {"characteristic": ("intelligence", "Int")},
+    "Sail": {"characteristic": ("agility", "Ag")},
+    "Secret Signs": {"characteristic": ("intelligence", "Int")},
+    "Set Trap": {"characteristic": ("dexterity", "Dex")},
+    "Sleight of Hand": {"characteristic": ("dexterity", "Dex")},
+    "Swim": {"characteristic": ("strength", "S")},
+    "Track": {"characteristic": ("initiative", "I")},
+    "Trade": {"characteristic": ("dexterity", "Dex")},
+}
+
+
+SKILL_DATA = {**BASIC_SKILL_DATA, **ADVANCED_SKILL_DATA}
--- src/wfrp/character/views/character.py
+++ src/wfrp/character/views/character.py
@@ -1,6 +1,7 @@
 from pyramid.view import view_config
 from pyramid.view import view_defaults
 
+from wfrp.character.skill_data import SKILL_DATA
 from wfrp.character.views.base_view import BaseView
 
 
@@ -8,4 +9,4 @@ from wfrp.character.views.base_view import BaseView
 class CharacterViews(BaseView):
     @view_config(request_method="GET", renderer="wfrp.character:templates/character.pt")
     def get_view(self):
-        return {"character": self.character}
+        return {"character": self.character, "skill_data": SKILL_DATA}
