6683eccbb4b2b3898565d1068ee2f47d19360a03
Michael Davis <m.r.davis@me.com>
2023-08-23 17:50:41 +0100

    Add fate and fortune

--- src/wfrp/character/views/advances.py
+++ src/wfrp/character/views/advances.py
@@ -59,6 +59,41 @@ class AdvancesViews(BaseView):
                 )
             )
         schema.add(advances_schema)
+        fate_schema = colander.SchemaNode(
+            colander.Mapping(),
+            name="fate & resilience",
+            validator=self.validate_fate,
+            description=(
+                f"You can spread {self.character.extra_points} points across "
+                "fate and fortune"
+            ),
+        )
+        fate_choices = [(i, i) for i in range(self.character.extra_points + 1)]
+        fate_schema.add(
+            colander.SchemaNode(
+                colander.Int(),
+                name="fate",
+                description=(f"Fate is currently {self.character.fate}"),
+                validator=colander.OneOf([x[0] for x in fate_choices]),
+                widget=deform.widget.RadioChoiceWidget(
+                    values=fate_choices, inline=True
+                ),
+                default=0,
+            )
+        )
+        fate_schema.add(
+            colander.SchemaNode(
+                colander.Int(),
+                name="resilience",
+                description=(f"Resilience is currently {self.character.resilience}"),
+                validator=colander.OneOf([x[0] for x in fate_choices]),
+                widget=deform.widget.RadioChoiceWidget(
+                    values=fate_choices, inline=True
+                ),
+                default=0,
+            )
+        )
+        schema.add(fate_schema)
         return schema
 
     def validate(self, form, values):
@@ -70,6 +105,25 @@ class AdvancesViews(BaseView):
             raise colander.Invalid(form, "You can only add a total of 5 advances")
         elif total < 5:
             raise colander.Invalid(form, "You have to add a total of 5 advances")
+
+    def validate_fate(self, form, values):
+        total = 0
+        for value in values:
+            if values[value]:
+                total += int(values[value])
+        total_allowed = self.character.extra_points
+        if total > total_allowed:
+            raise colander.Invalid(
+                form,
+                f"You can only spread {total_allowed} points between "
+                "fate and resilience",
+            )
+        elif total < self.character.extra_points:
+            raise colander.Invalid(
+                form,
+                f"You have to spread {total_allowed} points between "
+                "fate and resilience",
+            )
 
     @view_config(renderer="wfrp.character:templates/advances.pt")
     def form_view(self):
@@ -91,6 +145,16 @@ class AdvancesViews(BaseView):
                     current_value = getattr(self.character, attribute_lower)
                     new_value = current_value + captured["attributes"][advance]
                     setattr(self.character, attribute_lower, new_value)
+                self.character.fate = (
+                    self.character.fate + captured["fate & resilience"]["fate"]
+                )
+                self.character.resilience = (
+                    self.character.resilience
+                    + captured["fate & resilience"]["resilience"]
+                )
+                self.character.fortune = self.character.fate
+                self.character.resolve = self.character.resilience
+                self.character.extra_points = 0
                 url = self.request.route_url("species_skills", uuid=self.character.uuid)
                 self.character.status = {"species_skills": ""}
                 return HTTPFound(location=url)
--- tests/test_views_advances.py
+++ tests/test_views_advances.py
@@ -34,9 +34,15 @@ def test_submit_view(new_character):
     new_character.career = "Apothecary"
     new_character.status = {"advances": ""}
     old_toughness = new_character.toughness_initial = 40
+    new_character.extra_points = 3
+    assert new_character.fate == 0
+    assert new_character.fortune == 0
+    assert new_character.resilience == 0
+    assert new_character.resolve == 0
     request = testing.DummyRequest(
         post={
             "attributes": {"Toughness": "5", "Dexterity": "0", "Intelligence": "0"},
+            "fate & resilience": {"fate": "2", "resilience": "1"},
             "Accept_Advances": "Accept_Advances",
         }
     )
@@ -47,6 +53,11 @@ def test_submit_view(new_character):
     assert isinstance(response, HTTPFound)
     assert new_character.toughness_advances == 5
     assert new_character.toughness == old_toughness + 5
+    assert new_character.extra_points == 0
+    assert new_character.fate == 2
+    assert new_character.fortune == 2
+    assert new_character.resilience == 1
+    assert new_character.resolve == 1
 
 
 @pytest.mark.views
@@ -57,6 +68,28 @@ def test_invalid_submit_view(new_character):
     request = testing.DummyRequest(
         post={
             "attributes": {"Toughness": "3", "Dexterity": "2", "Intelligence": "1"},
+            "Accept_Advances": "Accept_Advances",
+        }
+    )
+    request.matched_route = DummyRoute(name="advances")
+    request.matchdict = {"uuid": new_character.uuid}
+    view = AdvancesViews(request)
+    response = view.form_view()
+    assert isinstance(response, dict)
+    assert "form" in response
+    assert "You can only add a total of 5 advances" in response["form"]
+
+
+@pytest.mark.views
+def test_invalid_fate_submit_view(new_character):
+    new_character.species = "Human"
+    new_character.career = "Apothecary"
+    new_character.extra_points = 3
+    new_character.status = {"advances": ""}
+    request = testing.DummyRequest(
+        post={
+            "attributes": {"Toughness": "3", "Dexterity": "2", "Intelligence": "1"},
+            "fate & resilience": {"fate": "2", "resilience": "2"},
             "Accept_Advances": "Accept_Advances",
         }
     )
