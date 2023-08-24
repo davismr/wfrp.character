d09030ab9b1d927ed9976fffcd7701a6576e8b11
Michael Davis <m.r.davis@me.com>
2023-08-24 15:54:53 +0100

    Add some missing weapons and armour

--- src/wfrp/character/armour_data.py
+++ src/wfrp/character/armour_data.py
@@ -1,9 +1,23 @@
 ARMOUR_DATA = {
     "Leather Jack": {
         "Price": {},
-        "Enc": 0,
+        "Enc": 1,
         "Availability": "Common",
         "Locations": ["Arms", "Body"],
+        "APs": 1,
+    },
+    "Leather Jerkin": {
+        "Price": {},
+        "Enc": 1,
+        "Availability": "Common",
+        "Locations": ["Body"],
+        "APs": 1,
+    },
+    "Leather Leggings": {
+        "Price": {},
+        "Enc": 1,
+        "Availability": "Common",
+        "Locations": ["Legs"],
         "APs": 1,
     },
     "Leather Breastplate": {
@@ -13,5 +27,13 @@ ARMOUR_DATA = {
         "Locations": ["Body"],
         "APs": 2,
         "Qualities": ["Weakpoints"],
+    },
+    "Mail Shirt": {
+        "Price": {},
+        "Enc": 2,
+        "Availability": "Scarce",
+        "Locations": ["Body"],
+        "APs": 2,
+        "Qualities": ["Flexibile", "Stealth -10"],
     },
 }
--- src/wfrp/character/weapons_data.py
+++ src/wfrp/character/weapons_data.py
@@ -23,6 +23,55 @@ MELEE_WEAPONS_DATA = {
         "Reach": "Personal",
         "Damage": ["SB", 2],
     },
+    "Flail": {
+        "Group": "Flail",
+        "Price": {},
+        "Enc": 1,
+        "Availability": "Scarce",
+        "Reach": "Average",
+        "Damage": ["SB", 5],
+        "Qualities": ["Distract", "Wrap"],
+    },
+    "Quarterstaff": {
+        "Group": "Polearm",
+        "Price": {},
+        "Enc": 2,
+        "Availability": "Common",
+        "Reach": "Long",
+        "Damage": ["SB", 4],
+        "Qualities": ["Defensive", "Pummel"],
+    },
 }
 
-WEAPONS_DATA = MELEE_WEAPONS_DATA
+RANGED_WEAPONS_DATA = {
+    "Crossbow": {
+        "Group": "Crossbow",
+        "Price": {},
+        "Enc": 2,
+        "Availability": "Common",
+        "Reach": 60,
+        "Damage": [9],
+        "Qualities": ["Reload 1"],
+    },
+    "Whip": {
+        "Group": "Entangling",
+        "Price": {},
+        "Enc": 0,
+        "Availability": "Common",
+        "Reach": 60,
+        "Damage": ["SB", 9],
+        "Qualities": ["Entangle"],
+    },
+    "Sling": {
+        "Group": "Sling",
+        "Price": {},
+        "Enc": 0,
+        "Availability": "Common",
+        "Reach": 60,
+        "Damage": [6],
+    },
+}
+
+WEAPONS_DATA = {}
+WEAPONS_DATA.update(MELEE_WEAPONS_DATA)
+WEAPONS_DATA.update(RANGED_WEAPONS_DATA)
