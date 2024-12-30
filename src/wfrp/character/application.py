dd013687c85ae37a7e51360f385d6f092e8716f2
Michael Davis <m.r.davis@me.com>
2024-12-30 10:28:03 -0600

    DO NOT MERGE: test google auth

--- src/wfrp/character/application.py
+++ src/wfrp/character/application.py
@@ -1,4 +1,13 @@
+from typing import List
+from typing import NamedTuple
+
 from pyramid.config import Configurator
+from pyramid.response import Response
+from pyramid.security import Allowed
+from pyramid.security import Denied
+from pyramid.session import SignedCookieSessionFactory
+from pyramid.view import view_config
+from pyramid_googleauth import GoogleSecurityPolicy
 from sqlalchemy import engine_from_config
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import scoped_session
@@ -10,6 +19,34 @@ register(DBSession)
 Base = declarative_base()
 
 
+class TestGoogleSecurityPolicy(GoogleSecurityPolicy):
+    class Identity(NamedTuple):
+        permissions: List[str]
+
+    def identity(self, request):
+        userid = self.authenticated_userid(request)
+        if userid:
+            return self.Identity(permissions=["google"])
+        return self.Identity([])
+
+    def permits(self, request, _context, permission):
+        if permission in self.identity(request).permissions:
+            return Allowed("allowed")
+        return Denied("denied")
+
+
+@view_config(route_name="protected", request_method="GET", permission="google")
+def protected(request):
+    return Response(
+        "<p>You have to be logged in to see this page.</p>"
+        f"<p>Your authenticated_userid is: <code>{request.authenticated_userid}"
+        "</code>.</p>"
+        f"<p>Your permissions are: <code>{request.identity.permissions}</code>.</p>"
+        f'<p><a href="{ request.route_url("pyramid_googleauth.logout") }">'
+        "Log out</a></p>"
+    )
+
+
 def configure_app(global_config, **settings):
     """Configure the pyramid api."""
     engine = engine_from_config(settings, "sqlalchemy.")
@@ -18,6 +55,14 @@ def configure_app(global_config, **settings):
     if "wfrp.character.enable_auth" not in settings:
         settings["wfrp.character.enable_auth"] = False
     enable_auth = settings.get("wfrp.character.enable_auth")
+    settings["pyramid_googleauth.google_client_id"] = (
+        "818352804815-dgeu215p713i1rllt3ee1vdn4v9k6hq2.apps.googleusercontent.com"
+    )
+    settings["pyramid_googleauth.google_client_secret"] = (
+        "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"
+    )
+    settings["pyramid_googleauth.secret"] = "secret"
+    settings["pyramid_googleauth.login_success_redirect_url"] = "/protected"
     config = Configurator(settings=settings)
     # to prevent circular imports
     if enable_auth is True:
@@ -28,7 +73,11 @@ def configure_app(global_config, **settings):
                 secret=settings["wfrp.character.secret"],
             ),
         )
+    config.set_session_factory(SignedCookieSessionFactory("secret"))
+    config.set_security_policy(TestGoogleSecurityPolicy())
     config.include("wfrp.character.routes")
+    config.include("pyramid_googleauth")
+    config.add_route("protected", "/protected")
     config.add_static_view("static", "wfrp.character:static")
     config.add_static_view("static_deform", "deform:static")
     config.add_request_method(dbsession, reify=True)
