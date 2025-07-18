dd99d6c80f128784fe7e746d93a3c51b150d1de9
Michael Davis <m.r.davis@me.com>
2025-07-18 06:32:06 +0100

    Move tests

--- tests/test_edit_campaign.py
+++ tests/test_edit_campaign.py
@@ -4,6 +4,7 @@ from pyramid import testing
 from wfrp.character.application import DBSession
 from wfrp.character.application import dbsession
 from wfrp.character.forms.edit_campaign import CampaignEditViews
+from wfrp.character.models.campaign import Campaign
 
 
 @pytest.mark.edit
@@ -17,3 +18,37 @@ def test_form_view(campaign_factory):
     response = view.form_view()
     assert isinstance(response, dict)
     assert "form" in response
+
+
+@pytest.mark.campaign
+def test_form_view_new(testapp):
+    request = testing.DummyRequest()
+    request.dbsession = dbsession(request)
+    view = CampaignEditViews(request)
+    response = view.form_view()
+    assert isinstance(response, dict)
+    assert "form" in response
+    assert "Rough Nights and Hard Days" in response["form"]
+
+
+@pytest.mark.campaign
+def test_submit(testapp):
+    DBSession.query(Campaign).delete()
+    request = testing.DummyRequest(
+        post={
+            "campaign_name": "My Awesome Campaign",
+            "expansions": ["rough_nights", "sea_of_claws"],
+            "Save": "Save",
+        }
+    )
+    request.dbsession = dbsession(request)
+    campaigns = request.dbsession.query(Campaign).all()
+    assert len(campaigns) == 0
+    view = CampaignEditViews(request)
+    response = view.form_view()
+    assert response.status_code == 302
+    assert response.location == "/"
+    campaigns = request.dbsession.query(Campaign).all()
+    assert len(campaigns) == 1
+    assert campaigns[0].name == "My Awesome Campaign"
+    assert campaigns[0].expansions.sort() == ["rough_nights", "sea_of_claws"].sort()
