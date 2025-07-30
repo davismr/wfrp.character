74a894625a476bfcd12a95c0d9bc9fc805b48da8
Michael Davis <m.r.davis@me.com>
2025-07-30 21:05:09 +0100

    Add ability to delete account

--- src/wfrp/character/views/account.py
+++ src/wfrp/character/views/account.py
@@ -1,7 +1,20 @@
+import colander
+import deform
+from pyramid.httpexceptions import HTTPFound
+from pyramid.httpexceptions import HTTPUnauthorized
+from pyramid.security import forget
 from pyramid.view import view_config
 from pyramid.view import view_defaults
+from sqlalchemy.exc import NoResultFound
 
-from wfrp.character.models.character import Character
+from wfrp.character.models.user import User
+
+
+def confirm_delete_validator(node, value):
+    if not value:
+        raise colander.Invalid(
+            node, "You have to confirm that you want to delete your account"
+        )
 
 
 @view_defaults(route_name="account")
@@ -9,12 +22,77 @@ class AccountPageViews:
     def __init__(self, request):
         self.request = request
         self.logged_in = request.authenticated_userid
+        try:
+            self.user = (
+                self.request.dbsession.query(User)
+                .filter(User.email == self.logged_in)
+                .one()
+            )
+        except NoResultFound:
+            raise HTTPUnauthorized
+
+    def schema(self):
+        schema = colander.SchemaNode(
+            colander.Mapping(),
+            title="Delete My Account",
+        )
+        return schema
 
     @view_config(request_method="GET", renderer="wfrp.character:templates/account.pt")
     def get_view(self):
-        characters = self.request.dbsession.query(Character).all()
-        character_list = {}
-        for character in characters:
-            url = self.request.route_url("character_summary", id=character.id)
-            character_list[url] = character.get_display_title()
-        return {"characters": character_list}
+        schema = self.schema()
+        form = deform.Form(
+            schema,
+            buttons=("Delete Account",),
+        )
+        html = form.render()
+        static_assets = self.get_widget_resources(form)
+        return {
+            "form": html,
+            "css_links": static_assets["css"],
+            "js_links": static_assets["js"],
+        }
+
+    @view_config(
+        request_method="POST", renderer="wfrp.character:templates/account_delete.pt"
+    )
+    def post_view(self):
+        schema = self.schema()
+        schema.add(
+            colander.SchemaNode(
+                colander.Boolean(),
+                widget=deform.widget.CheckboxWidget(),
+                name="confirm_delete",
+                title="Delete my account",
+                label="Confirm I want to delete my account",
+                validator=confirm_delete_validator,
+            )
+        )
+        form = deform.Form(
+            schema,
+            buttons=("Confirm Account",),
+        )
+        if "Confirm_Account" in self.request.POST:
+            try:
+                form.validate(self.request.POST.items())
+            except deform.ValidationFailure as error:
+                html = error.render()
+            else:
+                for character in self.user.characters:
+                    self.request.dbsession.delete(character)
+                self.request.dbsession.delete(self.user)
+                forget(self.request)
+                return HTTPFound(location="/")
+        else:
+            html = form.render()
+        static_assets = self.get_widget_resources(form)
+        return {
+            "form": html,
+            "css_links": static_assets["css"],
+            "js_links": static_assets["js"],
+        }
+
+    def get_widget_resources(self, form):
+        static_assets = form.get_widget_resources()
+        static_assets["css"].append("deform:static/css/form.css")
+        return static_assets
---
+++ tests/test_account_delete.py
@@ -0,0 +1,30 @@
+import pytest
+from pyramid import testing
+
+from wfrp.character.application import DBSession
+from wfrp.character.application import dbsession
+from wfrp.character.models.character import Character
+from wfrp.character.models.user import User
+from wfrp.character.views.account import AccountPageViews
+
+
+@pytest.mark.account
+def test_account_delete(new_character):
+    DBSession.query(User).delete()
+    DBSession.query(Character).delete()
+    new_user = User()
+    new_user.name = "User to Delete"
+    DBSession.add(new_user)
+    new_user = DBSession.query(User).filter_by(name="User to Delete").first()
+    new_character.user_id = new_user.id
+    assert new_character.user == new_user
+    request = testing.DummyRequest(
+        post={"confirm_delete": "true", "Confirm_Account": "Confirm_Account"}
+    )
+    request.dbsession = dbsession(request)
+    view = AccountPageViews(request)
+    response = view.post_view()
+    assert response.status_code == 302
+    assert response.location == "/"
+    assert DBSession.query(User).all() == []
+    assert DBSession.query(Character).all() == []
