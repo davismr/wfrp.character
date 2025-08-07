0ec455b403e61592f1aedd27d3602e891c5e7910
Michael Davis <m.r.davis@me.com>
2025-08-07 22:22:29 +0100

    Convert to union operators for dicts
    which means now dependent on python 3.9

--- src/wfrp/character/data/careers/careers.py
+++ src/wfrp/character/data/careers/careers.py
@@ -7,13 +7,13 @@ from wfrp.character.data.careers.riverfolk import RIVERFOLK_CLASS_DATA
 from wfrp.character.data.careers.rogues import ROGUES_CLASS_DATA
 from wfrp.character.data.careers.warriors import WARRIORS_CLASS_DATA
 
-CAREER_DATA = {
-    **ACADEMIC_CLASS_DATA,
-    **BURGHERS_CLASS_DATA,
-    **COURTIERS_CLASS_DATA,
-    **PEASANTS_CLASS_DATA,
-    **RANGERS_CLASS_DATA,
-    **RIVERFOLK_CLASS_DATA,
-    **ROGUES_CLASS_DATA,
-    **WARRIORS_CLASS_DATA,
-}
+CAREER_DATA = (
+    ACADEMIC_CLASS_DATA
+    | BURGHERS_CLASS_DATA
+    | COURTIERS_CLASS_DATA
+    | PEASANTS_CLASS_DATA
+    | RANGERS_CLASS_DATA
+    | RIVERFOLK_CLASS_DATA
+    | ROGUES_CLASS_DATA
+    | WARRIORS_CLASS_DATA
+)
