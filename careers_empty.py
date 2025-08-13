1a60adfe8bbcc4fc8b11d15b8016fc9c06178bf4
Michael Davis <m.r.davis@me.com>
2025-08-13 08:05:36 +0100

    DO NOT MERGE: empty dict

---
+++ careers_empty.py
@@ -0,0 +1,87 @@
+SEAFARER_DATA = {
+    "Beachcomber": {
+        "class": "Seafarer",
+        "Scavenger": {
+            "status": {"tier": "Brass", "standing": 1},
+            "attributes": ["", "", ""],
+            "skills": [
+                "",
+                "",
+                "",
+                "",
+                "",
+                "",
+                "",
+                "",
+                "",
+                "",
+            ],
+            "talents": [
+                "",
+                "",
+                "",
+                "",
+            ],
+            "trappings": [
+                "",
+            ],
+        },
+        "Beachcomber": {
+            "status": {"tier": "Brass", "standing": 3},
+            "attributes": [""],
+            "skills": [
+                "",
+                "",
+                "",
+                "",
+                "",
+                "",
+            ],
+            "talents": [
+                "",
+                "",
+                "",
+                "",
+            ],
+            "trappings": [
+                "",
+            ],
+        },
+        "Drifter": {
+            "status": {"tier": "Brass", "standing": 5},
+            "attributes": [""],
+            "skills": [
+                "",
+                "",
+                "",
+                "",
+            ],
+            "talents": [
+                "",
+                "",
+                "",
+                "",
+            ],
+            "trappings": [
+                "",
+            ],
+        },
+        "Master of the Beach": {
+            "status": {"tier": "Silver", "standing": 1},
+            "attributes": [""],
+            "skills": [
+                "",
+                "",
+            ],
+            "talents": [
+                "",
+                "",
+                "",
+                "",
+            ],
+            "trappings": [
+                "",
+            ],
+        },
+    },
+}
