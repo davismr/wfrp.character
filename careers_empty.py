9a11bca985fd918b03aa5c1d8386850104400e1b
Michael Davis <m.r.davis@me.com>
2025-08-14 08:17:04 +0100

    WiP

--- careers_empty.py
+++ careers_empty.py
@@ -85,3 +85,20 @@ SEAFARER_DATA = {
         },
     },
 }
+
+"": {
+    "max": 1,
+    "tests": "",
+    "short_description": "",
+    "description": (
+        ""
+    ),
+},
+
+['Unshakeable',
+ 'Savant(Medicine)',
+ 'trong-minded',
+ 'Master Orator',
+ 'Master Orator',
+ 'Arcane Magic',
+ 'Magical Sense']
