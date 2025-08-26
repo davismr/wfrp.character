2ed1b5b92feea875c7cae0ce5829c834e311f7c7
Michael Davis <m.r.davis@me.com>
2025-08-26 05:50:28 +0100

    Embed css as files to weasyprint

--- src/wfrp/character/views/character.py
+++ src/wfrp/character/views/character.py
@@ -1,4 +1,5 @@
 import logging
+from pathlib import Path
 
 from pyramid.renderers import render_to_response
 from pyramid.response import Response
@@ -13,7 +14,9 @@ from wfrp.character.views.base_view import BaseView
 logger = logging.getLogger(__name__)
 
 try:
+    from weasyprint import CSS
     from weasyprint import HTML
+    from weasyprint.text.fonts import FontConfiguration
 
     WEASYPRINT_INSTALLED = True
 except (ImportError, OSError):
@@ -42,10 +45,31 @@ class CharacterViews(BaseView):
 
     @view_config(route_name="pdf-print")
     def pdf_print(self):
+        font_config = FontConfiguration()
+        css_folder = Path(__file__).parent.parent / "static/"
+        with open(css_folder / "wfrp.css") as file:
+            css = file.read()
+        with open(css_folder / "print.css") as file:
+            css += file.read()
+        font_folder = css_folder / "fonts/"
+        css += (
+            "@font-face {"
+            "font-family: 'Caslon Antique';"
+            f"src: url('file://{font_folder}/caslon-antique.regular.ttf'),"
+            f"url('file://{font_folder}/caslon-antique.bold.ttf'),"
+            f"url('file://{font_folder}/caslon-antique.italic.ttf'),"
+            f"url('file://{font_folder}/caslon-antique.bolditalic.ttf')"
+            "}"
+        )
+        css = CSS(string=css, font_config=font_config)
         html = render_to_response(
             "wfrp.character:templates/pdf.pt", self.full_view(), request=self.request
         )
         filename = f"{self.character.get_filename()}.pdf"
-        response = Response(body=HTML(string=html.body).write_pdf())
+        response = Response(
+            body=HTML(string=html.body).write_pdf(
+                stylesheets=[css], font_config=font_config
+            )
+        )
         response.headers["Content-Disposition"] = f"attachment;filename={filename}"
         return response
