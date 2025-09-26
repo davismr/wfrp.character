91fabfcd3f4d83fc0ba5d3e5ece9285ed2c08e59
Michael Davis <m.r.davis@me.com>
2025-09-26 18:55:53 +0100

    Add petty magic spells

---
+++ src/wfrp/character/data/magic/petty.py
@@ -0,0 +1,257 @@
+PETTY_MAGIC_DATA = {
+    "Animal Friend": {
+        "range": "1 yard",
+        "target": 1,
+        "duration": "1 hour",
+        "description": (
+            "You make friends with a creature that is smaller than you and possesses "
+            "the Bestial Creature Trait. The animal trusts you completely and regards "
+            "you as a friend."
+        ),
+    },
+    "Bearings": {
+        "range": "You",
+        "target": "You",
+        "duration": "Instant",
+        "description": (
+            "You sense the influx of the Winds of Magic from their source. You know "
+            "which direction North is."
+        ),
+    },
+    "Dazzle": {
+        "range": "Touch",
+        "target": 1,
+        "duration": "Willpower Bonus Rounds",
+        "description": (
+            "The target gains 1 Blinded Condition, and gains 1 Blinded Condition at "
+            "the start of each round for the duration of the spell."
+        ),
+    },
+    "Careful Step": {
+        "range": "You",
+        "target": "You",
+        "duration": "WP minutes",
+        "description": (
+            "The magic flowing through your feet ensures any organic matter you tread "
+            "upon remains undamaged: twigs do not break, grass springs back to its "
+            "original position, and even delicate flowers are unharmed. Those seeking "
+            "to use the Track skill to pursue you through rural terrain suffer a –30 "
+            "penalty to their Tests."
+        ),
+    },
+    "Conserve": {
+        "range": "1 yard",
+        "target": 1,
+        "duration": "Willpower Bonus days",
+        "description": (
+            "You preserve up to a day’s worth of rations. During this time they will "
+            "not rot, develop mould, or go stale, although they can still be harmed by "
+            "external factors, such as getting wet or being burned or poisoned."
+        ),
+    },
+    "Dart": {
+        "range": "Willpower yards",
+        "target": 1,
+        "duration": "Instant",
+        "description": (
+            "You cause a small dart of magical energy to fly from your fingers. This "
+            "is a magic missile with a Damage of +0."
+        ),
+    },
+    "Drain": {
+        "range": "Touch",
+        "target": 1,
+        "duration": "Instant",
+        "description": (
+            "You touch your targets, draining their life. This counts as a magic "
+            "missile with Damage +0 that ignores Armour Points. You then Heal 1 Wound."
+        ),
+    },
+    "Eavesdrop": {
+        "range": "Willpower yards",
+        "target": 1,
+        "duration": "Initiative Bonus minutes",
+        "description": (
+            "You can hear what your targets say as if you were standing right next "
+            "to them."
+        ),
+    },
+    "Gust": {
+        "range": "Willpower yards",
+        "target": "Special",
+        "duration": "Instant",
+        "description": (
+            "You create a brief gust of wind, strong enough to blow out a candle, "
+            "cause an open door to slam, or blow a few pages to the floor."
+        ),
+    },
+    "Light": {
+        "range": "You",
+        "target": "You",
+        "duration": "Willpower minutes",
+        "description": (
+            "You create a small light, roughly equivalent to a torch, which glows from "
+            "your hand, staff or some other part of your person. While the spell is "
+            "active, you may choose to increase the illumination to that of a lantern, "
+            "or decrease it to that of a candle, if you pass a Average (+20) "
+            "Channelling Test."
+        ),
+    },
+    "Magic Flame": {
+        "range": "You",
+        "target": "You",
+        "duration": "Willpower Bonus Rounds",
+        "description": (
+            "You kindle a small flame that flickers to life in the palm of your hand. "
+            "It will not burn you, but will emit heat and set flammable objects "
+            "alight, like a natural flame."
+        ),
+    },
+    "Marsh Lights": {
+        "range": "Willpower yards",
+        "target": "Special",
+        "duration": "Willpower minutes",
+        "description": (
+            "You create a number of flickering magical lights up to your Intelligence "
+            "Bonus. They resemble torches or hooded lanterns. Providing they remain "
+            "within line of sight, for your Action you may control the lights by "
+            "passing a Average (+20) Channelling Test; a success allows you to send "
+            "the lights moving in any direction. They will move at walking pace in a "
+            "straight line, passing through any objects (or witnesses) in their path, "
+            "unless you test again to change their direction."
+        ),
+    },
+    "Murmured Whisper": {
+        "range": "Willpower yards",
+        "target": "Special",
+        "duration": "Willpower Bonus Rounds",
+        "description": (
+            "You cast your voice at a point within Willpower yards, regardless of line "
+            "of sight. Your voice sounds from this point, and all within earshot will "
+            "hear it."
+        ),
+    },
+    "Open Lock": {
+        "range": "Touch",
+        "target": "Special",
+        "duration": "Instant",
+        "description": ("One non-magical lock you touch opens."),
+    },
+    "Produce Small Animal": {
+        "range": "Touch",
+        "target": "Special",
+        "duration": "Instant",
+        "description": (
+            "You reach into a bag, pocket, or hat, or under a rock, bush or burrow, "
+            "producing a small animal of a type you would expect to find in the "
+            "vicinity, such as a rabbit, dove, or rat. If there are no appropriate "
+            "local animals, the spell does nothing. The temperament of the animal is "
+            "not guaranteed."
+        ),
+    },
+    "Protection from Rain": {
+        "range": "You",
+        "target": "You",
+        "duration": "Toughness Bonus hours",
+        "description": (
+            "You can keep yourself dry whatever the weather, unaffected by "
+            "precipitation. This affects rain, hail, sleet and snow, and any similar "
+            "water falling from the heavens, but not standing water."
+        ),
+    },
+    "Purify Water": {
+        "range": "1 yard",
+        "target": "Special",
+        "duration": "Instant",
+        "description": (
+            "You purify all water within a receptacle, such as a water flask, stein, "
+            "or jug. All non-magical impurities, such as poison or contaminants are "
+            "removed, leaving crisp, clear, potable water. If the vessel contained "
+            "another liquid that is predominantly water – such as ale, or wine – this "
+            "is also purified, turning into delicious, pure, non-alcoholic water."
+        ),
+    },
+    "Rot": {
+        "range": "1 yard",
+        "target": "Special",
+        "duration": "Instant",
+        "description": (
+            "You cause a roughly fist-sized volume of organic material to immediately "
+            "rot. Food stuffs perish, clothes crumble, leathers shrivel (losing 1 "
+            "Armour Point on 1 hit location), and similar, as dictated by the GM."
+        ),
+    },
+    "Sleep": {
+        "range": "Touch",
+        "target": 1,
+        "duration": "Willpower Bonus Rounds",
+        "description": (
+            "You touch your opponent, sending them into a deep sleep. If the target "
+            "has the Prone Condition, they gain the Unconscious Condition as they fall "
+            "asleep. They remain unconscious for the duration, although loud noises or "
+            "being moved or jostled will awaken them instantly. If your targets are "
+            "standing or sitting when affected, they start themselves awake as they "
+            "hit the ground, gaining the Prone Condition, but remaining conscious. If "
+            "your targets are not resisting, and are suitably tired, they will, at the "
+            "spell’s end, pass into a deep and restful sleep."
+        ),
+    },
+    "Spring": {
+        "range": "Touch",
+        "target": "Special",
+        "duration": "Willpower Bonus Rounds",
+        "description": (
+            "You touch the ground and water bubbles forth at the rate of 1 pint per "
+            "Round, to a total of your Initiative Bonus in pints."
+        ),
+    },
+    "Shock": {
+        "range": "Touch",
+        "target": 1,
+        "duration": "Instant",
+        "description": "Your target receives 1 Stunned Condition.",
+    },
+    "Sly Hands": {
+        "range": "You",
+        "target": "You",
+        "duration": "Willpower Bonus Rounds",
+        "description": (
+            "You teleport a small object — no bigger than your fist — from about your "
+            "person into your hand."
+        ),
+    },
+    "Sounds": {
+        "range": "Willpower yards",
+        "target": "Special",
+        "duration": "Willpower Bonus Rounds",
+        "description": (
+            "You create small noises nearby. You can create quiet, indistinct noises "
+            "that sound as if they come from a specific location within range, "
+            "regardless of line of sight. The noises can evoke something specific, "
+            "such as footsteps, whispers or the howl of an animal, but nothing so "
+            "distinct that it might convey a message. While the spell is active, you "
+            "may control the sounds by passing a Average (+20) Channelling Test. A "
+            "success allows you to move the sounds to another point within range, or "
+            "to increase or decrease their volume."
+        ),
+    },
+    "Twitch": {
+        "range": "Willpower Bonus yards",
+        "target": "Special",
+        "duration": "Instant",
+        "description": (
+            "You cause a small object to move, slightly. Something may fall from a "
+            "shelf, or a book may slam its pages shut. If the object is held, the "
+            "holder must pass an Average (+20) Dexterity Test or drop the object."
+        ),
+    },
+    "Warning": {
+        "range": "1 yard",
+        "target": "Special",
+        "duration": "Instant",
+        "description": (
+            "You channel magic into an object, noticing immediately if it has been "
+            "poisoned or trapped."
+        ),
+    },
+}
